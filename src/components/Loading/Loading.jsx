e10fd57b2a4f933b3387cc6408717736d9a01138
kapi104 <skorupa7979@gmail.com>
2023-12-19 00:10:50 +0100

    styled Loading component

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,10 +1,11 @@
 import Icon from '@mdi/react';
 import { mdiLoading } from '@mdi/js';
+import styles from './Loading.module.scss';
 
 const Loading = () => {
   return (
-    <div data-testid="loading">
-      <Icon path={mdiLoading}></Icon>
+    <div data-testid="loading" className={styles.loading}>
+      <Icon path={mdiLoading} color="grey"></Icon>
     </div>
   );
 };
